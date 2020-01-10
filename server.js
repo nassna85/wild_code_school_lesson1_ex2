ed933446a20f02a1c609c0cba009c38c6f66a594
Naïm <nassna85@hotmail.fr>
2020-01-10 22:15:32 +0100

    Install Chalk and display names using chalk

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const chalk = require("chalk");
+const log = console.log;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Hello World");
+  res.end("See the console!!!");
 });
+
+log(
+  chalk.blue("Kedji") +
+    " " +
+    chalk.cyan("Boris") +
+    " " +
+    chalk.red("Gaelle") +
+    " " +
+    chalk.green("Julien") +
+    " " +
+    chalk.grey("Maxime")
+);
 
 const port = 3000;
 
